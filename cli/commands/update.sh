99c8761819e1bda0bab6276308ac5bb002e77f98
mktcode <kontakt@markus-kottlaender.de>
2026-02-25 00:01:50 +0100

    Optimize update command to conditionally run npm commands based on git pull output

--- cli/commands/update.sh
+++ cli/commands/update.sh
@@ -9,9 +9,12 @@ cmd_update() {
     local domain_name=${1:-}
 
     cd "/var/www/websites/${domain_name}"
-    git pull
-    npm ci
-    npm run build
+    GIT_PULL_OUTPUT=$(git pull)
+    echo "$GIT_PULL_OUTPUT"
+    if echo "$GIT_PULL_OUTPUT" | grep -Eq '^ [1-9][0-9]* file(s)? changed'; then
+        npm ci
+        npm run build
+    fi
     chown -R websites:websites "/var/www/websites/${domain_name}"
     supervisorctl restart "${domain_name}"
 }
